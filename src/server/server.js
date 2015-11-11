129e9a2241e110a3214b91927bd3b643c4727e00
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2015-11-11 20:31:24 +0200

    Add object-rest-spread transform and use it in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -4,5 +4,6 @@ const port = process.env.PORT || 3000
 const host = 'localhost'
 
 const server = app.listen(port, host, () => {
-  console.log(`Listening on http://${host}:${port}`)
+  let { address, port } = server.address()
+  console.log(`Listening on http://${address}:${port}`)
 })
