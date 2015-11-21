f09e1b58266bcd39ec7be17df4972022afbf201e
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2015-11-21 18:48:27 +0200

    Add script view and extraBody param to baseBody view

--- src/server/views.js
+++ src/server/views.js
@@ -1,5 +1,6 @@
-/* eslint-disable standard/object-curly-even-spacing */
+import he from 'he'
 
+/* eslint-disable standard/object-curly-even-spacing */
 const basePage = (head, body) => `
 <!DOCTYPE html>
 <html lang="en">
@@ -18,18 +19,29 @@ ${extraHead}
 <title>${title}</title>
 `
 
-const baseBody = (content) => `
+const baseBody = (content, extraBody = '') => `
 <div id="content">${content}</div>
+${extraBody}
 <script src="/public/bundle.js"></script>
 `
 
-const page = (title, content) => basePage(baseHead(title), baseBody(content))
+const page = (title, content, { extraBody = '' } = {}) =>
+  basePage(baseHead(title), baseBody(content, extraBody))
 
 const errorPage = (errorMsg) => page('Internal server error!', `
 <h1>Internal server error</h1>
 <pre>${errorMsg}</pre>
 `)
 
-export default { basePage, baseHead, baseBody, page, errorPage}
+const script = ({ id = '', content = '', type = '', src = '' } = {}) => `
+<script${ifAttr('id', id)}${ifAttr('type', type)}${ifAttr('src', src)}>
+  ${he.encode(content)}
+</script>
+`
 
+export default { basePage, baseHead, baseBody, page, errorPage, script }
 /* eslint-enable standard/object-curly-even-spacing */
+
+function ifAttr (name, value) {
+  return value ? ` ${name}="${he.encode(value)}"` : ''
+}
